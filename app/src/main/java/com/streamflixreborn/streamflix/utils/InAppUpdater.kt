78d07823e4bd69248bba1c1e9977bfdbadcc563d
PaduU29 <PaduU29@users.noreply.github.com>
2026-07-03 08:31:59 +0200

    changed repo owner

--- app/src/main/java/com/streamflixreborn/streamflix/utils/InAppUpdater.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/utils/InAppUpdater.kt
@@ -14,8 +14,8 @@ import kotlin.math.max
 
 object InAppUpdater {
 
-    private const val GITHUB_OWNER = "streamflix-reborn"
-    private const val GITHUB_REPO = "streamflix"
+    private const val GITHUB_OWNER = "paduu29"
+    private const val GITHUB_REPO = "streamflix-revanced"
 
     private data class Version(val name: String) : Comparable<Version> {
         override operator fun compareTo(other: Version): Int {
